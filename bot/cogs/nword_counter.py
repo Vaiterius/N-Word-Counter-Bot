d14327f436ae9168cf92809531db6c7b1989ef8c
Gerard <gerard.gandionco@gmail.com>
2022-02-11 02:26:29 -0800

    Finish most core unit tests

--- bot/cogs/nword_counter.py
+++ bot/cogs/nword_counter.py
@@ -6,24 +6,34 @@ from pathlib import Path
 import pymongo
 from discord.ext import commands
 
-# Fetch bot token.
+# Fetch MongoDB token for database access.
 with Path("../config.json").open() as f:
     config = load(f)
-
-mongo_url = config["MONGO_URL"]
+    mongo_url = config["MONGO_URL"]
 
 
 class NWordCounter(commands.Cog):
-    """Command category for n-word occurrences"""
+    """Category for n-word occurrences
+    
+    TODO (✔️) (❌)
+    ❌ complete database functionality
+    ✔️ complete count_nwords unittest
+    ❌ comlpete pymongo unittest
+    ❌ implement server total n-words lookup command
+    ❌ implement voting system
+    ❌ implement single user n-word lookup
+    ❌ implement bigger table for count_nword translate method
+    """
 
     def __init__(self, bot):
         self.bot = bot
 
         # Initialize Mongo database.
-        # self.cluster = pymongo.MongoClient(mongo_url)
-        # self.db = self.cluster["NWordCounter"]
-        # self.collection = self.db["guild_users_db"]
+        self.cluster = pymongo.MongoClient(mongo_url)
+        self.db = self.cluster["NWordCounter"]
+        self.collection = self.db["guild_users_db"]
     
+
     @staticmethod
     def count_nwords(msg: str) -> int:
         """Return occurrences of n-words in a given message"""
@@ -38,42 +48,49 @@ class NWordCounter(commands.Cog):
 
         return count
     
+
     def guild_in_database(self, guild) -> bool:
         """Return True if guild is already recorded in database"""
         return self.collection.count_documents(
             {"guild_id": guild.id}
         ) != 0
     
+
     def create_database(self, guild) -> None:
-        """Initialize all guild params in database"""
-        # Upload base template.
+        """Initialize guild template in database"""
         self.collection.insert_one(
             {
                 "guild_id": guild.id,
                 "guild_name": guild.name,
-                "members": {}
+                "members": []
             }
         )
     
-    def member_in_database(self, guild_id, member) -> bool:
+
+    def member_in_database(self, guild, member) -> bool:
         """Return True if member is already recorded in guild database"""
-        return self.collection.find_one(
-            {"guild_id": guild_id}["members"]["name": member]
-        )
+        pass
     
-    def create_member(self, guild_id, member) -> None:
+
+    def create_member(self, guild, member) -> None:
         """Initialize member data in guild database"""
         self.collection.update(
-            {"guild_id": guild_id}, {
-                "$set": {
+            {"guild_id": guild.id}, {
+                "$push": {
                     "members": {
                         "name": member,
+                        "votes": 0,
                         "is_black": False,
                     }
                 }
             }
         )
     
+
+    def increment_nword_count(guild, member, count):
+        pass
+    
+
     @commands.Cog.listener()
     async def on_message(self, message):
         """Detect n-words"""
@@ -82,24 +99,34 @@ class NWordCounter(commands.Cog):
         
         guild = message.guild
         msg = message.content
-        author = message.author
+        author = message.author  # Should fetch user by ID instead of name.
 
         # Ensure guild has its own place in the database.
-        # if not self.guild_in_database(guild):
-        #     self.create_database(guild)
+        if not self.guild_in_database(guild):
+            self.create_database(guild)
         
         # Bot reaction to any n-word occurrence.
         num_nwords = self.count_nwords(msg)
         if num_nwords > 0:
 
-            # TODO
+            if not self.member_in_database(guild, author):
+                self.create_member(guild, author)
+            
+            self.increment_nword_count(guild, author, num_nwords)
 
             await message.reply(
                 f":camera_with_flash: Detected **{num_nwords}** n-words!"
             )
-    
-    # @commands.command()
+
+
+    @commands.command()
+    async def count(self, mention):
+        """Return total nword count of a person in a server"""
+        # If person is not found in server, abort.
+        # Else return embedded formatted number.
+        pass
 
 
 def setup(bot):
     bot.add_cog(NWordCounter(bot))
+
---
+++ bot/tests/test_pymongo_queries.py
@@ -0,0 +1,154 @@
+"""Unit test pymongo queries with nested documents.
+
+- MAYBE: use mongomock instead for testing collection objects
+"""
+import random
+import unittest
+from json import load
+from pathlib import Path
+from pprint import pprint
+
+import pymongo
+
+
+class TestPyMongo(unittest.TestCase):
+    """Test and ensure working pymongo functions before use"""
+
+    def setUp(self):
+        # Fetch token for MongoDB access.
+        with Path("../config.json").open() as f:
+            config = load(f)
+            mongo_url = config["MONGO_URL"]
+
+        # Initialize database for querying.
+        self.cluster = pymongo.MongoClient(mongo_url)
+        self.db = self.cluster["Tests"]
+        self.collection = self.db["test_guild_users"]
+
+        # Dummy guild templates - create and test diverse/different guilds.
+        self.guild_template1 = {
+            "guild_id": 1234,
+            "guild_name": "Guild1",
+            "members": []
+        }
+        self.guild_template2 = {
+            "guild_id": 4321,
+            "guild_name": "Guild2",
+            "members": []
+        }
+        self.guild_template3 = {
+            "guild_id": 1324,
+            "guild_name": "Guild3",
+            "members": []
+        }
+
+        # Dummy people info.
+        self.test_many_members_list1 = ["John", "Jane", "Jack"]
+        self.test_many_members_list2 = [
+            "George", "Dominic", "Page", "Drew", "Bartholomew", "Melissa", "Pat",
+            "Jackson", "Matt", "Darnelius", "Kristin", "Leo", "Ruby", "Noah"
+        ]
+    
+
+    def tearDown(self):
+        self.collection.delete_one({"guild_id": self.guild_template1["guild_id"]})
+        self.collection.delete_one({"guild_id": self.guild_template2["guild_id"]})
+        self.collection.delete_one({"guild_id": self.guild_template3["guild_id"]})
+        # pass
+
+
+    def test_guild_creation(self):
+        self.collection.insert_one(self.guild_template1)
+        found_guild1 = self.collection.find_one(self.guild_template1)
+        self.assertTrue(found_guild1)
+
+
+    def test_guild_with_many_members(self):
+        self.collection.insert_one(self.guild_template2)
+
+        # Insert sample members into newly-created guild.
+        for member in self.test_many_members_list1:
+            self.collection.update_one(
+                {"guild_id": self.guild_template2["guild_id"]}, {
+                    "$push": {
+                        "members": {
+                            "name": member,
+                            "votes": 0,
+                            "is_black": False
+                        }
+                    }
+                }
+            )
+        
+        found_guild2 = self.collection.find_one({"guild_id": self.guild_template2["guild_id"]})
+        self.assertTrue(found_guild2)
+
+        num_members = len(found_guild2["members"])
+        self.assertEqual(num_members, len(self.test_many_members_list1))
+    
+
+    def test_update_member_info(self):
+        self.collection.insert_one(self.guild_template3)
+        for member in self.test_many_members_list2:
+            self.collection.update_one(
+                {"guild_id": self.guild_template3["guild_id"]}, {
+                    "$push": {
+                        "members": {
+                            "name": member,
+                            "votes": random.randint(0, 5),
+                            "is_black": random.choice([True, False])
+                        }
+                    }
+                }
+            )
+        
+        found_guild3 = self.collection.find_one({"guild_id": self.guild_template3["guild_id"]})
+        self.assertTrue(found_guild3), "Guild3 does not exist"
+
+        num_members = len(found_guild3["members"])
+        self.assertEqual(num_members, len(self.test_many_members_list2)), "Number of members added incorrect"
+
+        # Find and update specific member's data.
+        name_to_find = random.choice(self.test_many_members_list2)
+        print(f"Name to find: {name_to_find}")
+        changed_name = "Timothy"
+        self.collection.update_one(
+            {
+                "members.name": name_to_find
+            },
+            {
+                "$set": {
+                    "members.$.name": changed_name
+                }
+            },
+            upsert=False
+        )
+
+        # Find and return specific member's data.
+        pipeline = [
+            {
+                "$match": {
+                    "guild_id": self.guild_template3["guild_id"],
+                    "members.name": changed_name
+                }
+            },
+            {
+                "$unwind": "$members"
+            },
+            {
+                "$match": {"members.name": changed_name}
+            },
+            {
+                "$replaceWith": "$members"
+            }
+        ]
+        name_found = self.collection.aggregate(pipeline).next()
+        pprint(name_found)
+        self.assertEqual(changed_name, name_found["name"]), f"{name_to_find} does not equal {name_found['name']}"
+
+
+    #
+
+
+if __name__ == "__main__":
+    unittest.main()
